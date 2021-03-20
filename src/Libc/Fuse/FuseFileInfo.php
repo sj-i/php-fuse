d813c99e50b139dd582b887f931143b6d792e58e
sji <sji@sj-i.dev>
2021-03-20 15:36:05 +0900

    fix the file header in FuseFileInfo.php

--- src/Libc/Fuse/FuseFileInfo.php
+++ src/Libc/Fuse/FuseFileInfo.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This file is part of the sj-i/php-fuse package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
 
 namespace Fuse\Libc\Fuse;
 

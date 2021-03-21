bda1eadfe3024d392943a052b65ee62a1b84fc56
sji <sji@sj-i.dev>
2021-03-22 02:47:47 +0900

    add TypedCDataArray

--- src/FFI/TypedCDataDefaultImplementationTrait.php
+++ src/FFI/TypedCDataDefaultImplementationTrait.php
@@ -16,6 +16,11 @@ namespace Fuse\FFI;
 use FFI;
 use FFI\CData;
 use Fuse\Fuse;
+use PhpDocTypeReader\Context\IdentifierContextFactory;
+use PhpDocTypeReader\PhpDocTypeReader;
+use PhpDocTypeReader\Type\GenericType;
+use PhpDocTypeReader\Type\ObjectType;
+use ReflectionProperty;
 
 trait TypedCDataDefaultImplementationTrait
 {
@@ -29,11 +34,38 @@ trait TypedCDataDefaultImplementationTrait
             $type = $property->getType()->getName();
             if (is_a($type, TypedCDataInterface::class, true)) {
                 $self->$name = $type::fromCData($cdata->$name);
+            } elseif (is_a($type, TypedCDataArray::class, true)) {
+                $self->$name = $type::fromCData($cdata->$name, self::getElementTypeOfCDataArray($property));
             } else {
                 $self->$name = $cdata->$name;
             }
         }
         return $self;
+    }
+
+    private static function getElementTypeOfCDataArray(ReflectionProperty $property)
+    {
+        $doc_comment = $property->getDocComment();
+        if (!is_string($doc_comment)) {
+            throw new \LogicException('TypedCDataArray property needs type annotation');
+        }
+        $php_doc_type_reader = new PhpDocTypeReader();
+        $identifier_context_factory = new IdentifierContextFactory();
+        $identifier_context = $identifier_context_factory->createFromFile(
+            $property->getDeclaringClass()->getFileName()
+        );
+        $type = $php_doc_type_reader->getVarTypes($doc_comment, $identifier_context);
+        if (!($type instanceof GenericType)) {
+            throw new \LogicException('TypedCDataArray property must have a generic type annotation');
+        }
+        if (count($type->parameter_types) !== 1) {
+            throw new \LogicException('TypedCDataArray property must have one parameter type');
+        }
+        $parameter_type = $type->parameter_types[0];
+        if (!($parameter_type instanceof ObjectType)) {
+            throw new \LogicException('parameter type of TypedCDataArray property must be a object type');
+        }
+        return $parameter_type->class_name;
     }
 
     abstract public static function getCTypeName(): string;
@@ -42,6 +74,8 @@ trait TypedCDataDefaultImplementationTrait
     {
         foreach ($this as $key => $value) {
             if (is_a($value, TypedCDataInterface::class, true)) {
+                $cdata->$key = $value->toCData($cdata->$key);
+            } elseif (is_a($value, TypedCDataArray::class, true)) {
                 $cdata->$key = $value->toCData($cdata->$key);
             } else {
                 $cdata->$key = $this->$key;
@@ -53,7 +87,7 @@ trait TypedCDataDefaultImplementationTrait
     public static function newCData(): CData
     {
         $typename = static::getCTypeName();
-        $cdata = FFI::new($typename);
+        $cdata = Fuse::getInstance()->ffi->new($typename);
         $type = Fuse::getInstance()->ffi->type(
             $typename
         );
---
+++ src/FFI/TypedCDataWrapper.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-fuse package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fuse\FFI;
+
+use Closure;
+use FFI\CData;
+use PhpDocTypeReader\Context\IdentifierContextFactory;
+use PhpDocTypeReader\PhpDocTypeReader;
+use PhpDocTypeReader\Type\GenericType;
+use PhpDocTypeReader\Type\ObjectType;
+use ReflectionClass;
+
+final class TypedCDataWrapper
+{
+    public function createWrapper(callable $callable): \Closure
+    {
+        if (!is_array($callable) or !is_object($callable[0])) {
+            return Closure::fromCallable($callable);
+        }
+        $class = new ReflectionClass(get_class($callable[0]));
+        $method = $class->getMethod($callable[1]);
+
+        $input_converters = [];
+        $outout_converters = [];
+        foreach ($method->getParameters() as $parameter) {
+            $class = $parameter->getClass();
+            if (!is_null($class) and $class->isSubclassOf(TypedCDataInterface::class)) {
+                $fromCData = $class->getMethod('fromCData');
+                $input_converters[] = fn ($cdata) => $fromCData->invoke(null, $cdata);
+                $outout_converters[] = function ($typed_c_data, CData $toCData): CData {
+                    return $typed_c_data->toCData($toCData);
+                };
+            } elseif (!is_null($class) and $class->getName() === TypedCDataArray::class) {
+                $fromCData = $class->getMethod('fromCData');
+                $elementType = $this->getElementTypeForCDataArrayParameter($parameter);
+                $input_converters[] = fn ($cdata) => $fromCData->invoke(null, $cdata, $elementType);
+                $outout_converters[] = function ($typed_c_data_array, CData $toCData): CData {
+                    return $typed_c_data_array->toCData($toCData);
+                };
+            } else {
+                $input_converters[] = fn ($cdata) => $cdata;
+                $outout_converters[] = fn ($typed_c_data, $cdata) => $typed_c_data;
+            }
+        }
+
+        return function (...$args) use ($input_converters, $outout_converters, $callable) {
+            $new_args = [];
+            foreach ($input_converters as $key => $converter) {
+                $new_args[$key] = $converter($args[$key]);
+            }
+            $result = $callable(...$new_args);
+            foreach ($outout_converters as $key => $converter) {
+                $args[$key] = $converter($new_args[$key], $args[$key]);
+            }
+            return $result;
+        };
+    }
+
+    public function getElementTypeForCDataArrayParameter(\ReflectionParameter $parameter): string
+    {
+        $parameter_name = $parameter->getName();
+        $doc_comment = $parameter->getDeclaringFunction()->getDocComment();
+        $declaring_file_name = $parameter->getDeclaringFunction()->getFileName();
+        $identifier_context_factory = new IdentifierContextFactory();
+        $php_doc_type_reader = new PhpDocTypeReader();
+        $param_types = $php_doc_type_reader->getParamTypes(
+            $doc_comment,
+            $identifier_context_factory->createFromFile($declaring_file_name)
+        );
+        $parameter_type = $param_types[$parameter_name];
+        if (!($parameter_type instanceof GenericType)) {
+            throw new \LogicException('TypedCDataArray must have a generic type annotation');
+        }
+        $element_type = $parameter_type->parameter_types[0];
+        if (!($element_type instanceof ObjectType)) {
+            throw new \LogicException('TypedCDataArray must have a object parameter type');
+        }
+        return $element_type->class_name;
+    }
+}
